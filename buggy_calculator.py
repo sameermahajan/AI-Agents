104a95bb390d6831b05f2a8af3d4411f276660f0
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2026-05-21 09:19:14 +0530

    Add files via upload

---
+++ buggy_calculator.py
@@ -0,0 +1,10 @@
+# buggy_calculator.py
+
+def divide(a, b):
+    return a / b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = divide(num1, num2)
+print("Result is: " + result)
